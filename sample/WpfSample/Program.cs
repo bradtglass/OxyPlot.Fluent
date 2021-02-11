e90156ee99d8cd5446dbd6c70839664d7a7b0499
Brad Glass <brad.t.glass@gmail.com>
2021-02-11 19:06:35 +0000

    Add empty sample and WPF project

---
+++ sample/WpfSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WpfSample
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/OxyPlot.Fluent.Wpf/WpfExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows;
+using OxyPlot.Fluent.Configurators;
+
+namespace OxyPlot.Fluent.Wpf
+{
+    public static class WpfExtensions
+    {
+        public static Window Show(this FigureConfigurator figure)
+            => throw new NotImplementedException();
+    }
+}
